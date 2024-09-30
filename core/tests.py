958bc4e05ddb8ccfa6066e64711b3059310d36ad
Rian Silva <rianpsilva@hotmail.com>
2024-09-30 14:53:12 -0300

    Adicionando os testes

--- core/tests.py
+++ core/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 
 class HomeTest(TestCase):
+    def setUp(self):
+        self.response =self.client.get('/')
     def test_get(self):
-        response = self.client.get('/')
-        self.assertEqual(200, response.status_code)
-#aa
+        self.assertEqual(200, self.response.status_code)
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'index.html')
+    
