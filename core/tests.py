ddb12585baa5c4f057a9cb458e8a297c75d08d0f
Rian Silva <rianpsilva@hotmail.com>
2024-10-07 14:34:26 -0300

    first commit

---
+++ core/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response =self.client.get('/')
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'index.html')
+    
