379234050d3fae945fb99175b5f9a9eaf738cb43
Rian Silva <rianpsilva@hotmail.com>
2024-11-05 15:42:08 -0300

    alteracaocss

--- eventif/settings.py
+++ eventif/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+STATIC_ROOT = str(BASE_DIR / 'core/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
