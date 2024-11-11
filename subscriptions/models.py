b4f21994f2756a5b57b87d7569dc52c950f78656
Rian Silva <rianpsilva@hotmail.com>
2024-11-11 17:11:14 -0300

    FinalDeAulaAdminAdd

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField('Nome', max_length=100)
+    cpf = models.CharField('CPF' ,max_length=11)
+    email = models.EmailField('E-Mail')
+    phone = models.CharField('Telefone',max_length=20)
+    created_at = models.DateTimeField('Criado em',auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'inscrições'
+        verbose_name = 'inscrição'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
