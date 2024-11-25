01af86c51ede1d4e38779d33627aa5ad43b70be3
Rian Silva <rianpsilva@hotmail.com>
2024-11-25 16:58:27 -0300

    Extract urls

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url as r
 from subscriptions.forms import SubscriptionForm
 from subscriptions.models import Subscription
 from django.core import mail
@@ -32,7 +32,7 @@ def create(request):
 
 
 
-    return HttpResponseRedirect('/inscricao/{}/'.format(subscription.pk))
+    return HttpResponseRedirect(r('subscriptions:detail',subscription.pk))
 
 
 def new(request):
